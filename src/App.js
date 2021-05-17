a604204be4cc05936240890a9fdd746d5f69a3b4
selam <selamawittekleab.brhane@powercoders.org>
2021-05-17 17:11:18 +0200

    my project

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
+
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [favourites, setFavourites] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+
+	const getMovieRequest = async (searchValue) => {
+		const url = `https://www.superheroapi.com/api.php/176153607739180/search/${searchValue}`;
+		// const movieAPI = `http://www.omdbapi.com/?s=${searchValue}&apikey=15872c5b`
+
+	/* 	const getHero = axios.get(url)
+		const getMovie = axios.get(movieAPI)
+	 
+		axios.all([getHero, getMovie]).then(
+		   axios.spread((...allData) => {
+			  const allDataHero = allData[0].data.results[0].name
+			  const geHeroMovie = allData[1].data.Search[0].Poster
+	 
+			  setSuperheroData(allDataHero)
+			  setMovies(geHeroMovie)  
+	  
+			 console.log(allDataHero)
+			 console.log(geHeroMovie)
+			 console.log(allData)
+	 
+		   })
+		)
+	   } 
+	 
+	 
+		 useEffect(() => {
+		   fetchData(searchText)
+		}, [searchText]) */
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.results) {
+			setMovies(responseJson.results);
+		}
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	useEffect(() => {
+		const movieFavourites = JSON.parse(
+			localStorage.getItem('react-movie-app-favourites')
+		);
+
+		if (movieFavourites) {
+			setFavourites(movieFavourites);
+		}
+	}, []);
+
+	const saveToLocalStorage = (items) => {
+		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+	};
+
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
+
+	const removeFavouriteMovie = (movie) => {
+		const newFavouriteList = favourites.filter(
+			(favourite) => favourite.id !== movie.id
+		);
+
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
+
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Heros' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={movies}
+					handleFavouritesClick={addFavouriteMovie}
+					favouriteComponent={AddFavourites}
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favourites' />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={favourites}
+					handleFavouritesClick={removeFavouriteMovie}
+					favouriteComponent={RemoveFavourites}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/MovieList.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const MovieList = (props) => {
+	const FavouriteComponent = props.favouriteComponent;
+
+	return (
+		<>
+			{props.movies.map((movie, index) => (
+				<div className='image-container d-flex justify-content-start m-3'>
+
+              <div className="card-deck textcolor">
+				<div className="card">
+
+				    <div className="card-header headercard"><h3>{movie.name}</h3></div>
+
+					<img className="card-img-top" src={movie.image.url} alt="Hero image" />
+
+					<div className="card-body">
+
+					<div className="container">
+					<div className="row">
+						
+
+					</div>
+					</div>
+					
+
+					<div className="container">
+					<div className="row">
+
+					<h5 className="card-title col-12 ">FullName: <span className="lightgray">{movie.biography['full-name']}</span> </h5>
+					<p className="card-title col-12">{movie.biography['alter-egos']}</p>
+
+						<h5 className="col-12">Stats</h5>
+					   <p className="col-4">Intelligence: {movie.powerstats.intelligence}</p> 
+						<p className="col-4">strength: {movie.powerstats.strength}</p>
+						<p className="col-4">speed: {movie.powerstats.speed}</p>
+						<p className="col-4">power: {movie.powerstats.power}</p>
+						<p className="col-4">durability: {movie.powerstats.durability}</p>
+						<p className="col-4">Combat: {movie.powerstats.combat}</p>
+
+						<h5 className="col-12">Work</h5>
+					    <p className="col-12">{movie.work.occupation}</p> 
+
+						<h5 className="col-12">Base</h5>
+					    <p className="col-12">{movie.work.base}</p> 
+						
+					</div>
+					</div>
+					
+					</div>
+				</div>
+				</div>
+
+					<div
+						onClick={() => props.handleFavouritesClick(movie)}
+						className='overlay d-flex align-items-center justify-content-center'
+					>
+						<FavouriteComponent />
+					</div>
+				</div>
+			))}
+		</>
+	);
+};
+
+export default MovieList;
+
